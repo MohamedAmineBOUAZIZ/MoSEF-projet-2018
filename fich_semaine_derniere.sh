6f24c70890922bd244d7774622014bc23de93d1b
MohamedAmineBOUAZIZ <amineebouaziz@gmail.com>
2018-12-03 18:54:39 +0100

    Première modification du Script en l'éditant sur la nouvelle branche

--- fich_semaine_derniere.sh
+++ fich_semaine_derniere.sh
@@ -8,3 +8,6 @@ echo "Se postionner dans le répertoire choisi:"
 cd $Rep
 echo "Afficher le contenu du répertoire choisi:"
 ls
+echo "Afficher tous les fichiers, d'un répertoire choisi préalablement, qui ont subi des modifications il y a moins d'une semaine:"
+find -type f -mtime -7
+
